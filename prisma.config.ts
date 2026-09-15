f37e8547e0eb9fb104dcf585772c66e875973dd9
Ivan <ivanjameslo02@gmail.com>
2026-09-15 16:29:56 +0800

    refactor: update shadow database URL configuration for conditional assignment

--- prisma.config.ts
+++ prisma.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
   },
   datasource: {
     url: env("DATABASE_URL"),
-    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+
+    ...(process.env.SHADOW_DATABASE_URL
+      ? {
+          shadowDatabaseUrl:
+            process.env.SHADOW_DATABASE_URL,
+        }
+      : {}),
   },
 });
